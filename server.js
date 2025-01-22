73970ab1617107cd19f0c29e0da868f154a213c3
PrinceJoshwa <sprincejoshwa85@gmail.com>
2025-01-23 02:08:18 +0530

    first commit

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['https://crypto-price-tracker-client.vercel.app', 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
